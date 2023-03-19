76538990872cce362f97894b93339b21fe3a5485
Yaghoub Ghadri <y.ghaderi@outlook.com>
2023-03-19 18:59:10 +0330

    dev take TSE WL

--- src/metafid/data/tse.py
+++ src/metafid/data/tse.py
@@ -1,6 +1,45 @@
 import pandas as pd
 import jdatetime
 import requests
+import re
+
+
+ua_list = [
+    "شپنا",
+    "خودرو",
+    "فملي",
+    "شستا",
+    "وبصادر",
+    "فولاد",
+    "خگستر",
+    "سلام",
+    "دي",
+    "سرو",
+    "خاور",
+    "وبملت",
+    "خساپا",
+    "فخوز",
+    "فرابورس",
+    "اهرم",
+    "حسير",
+    "خپارس",
+    "هاي وب",
+    "چكاپا",
+    "توسن",
+    "وتجارت",
+    "ذوب",
+    "بساما",
+    "شبندر",
+    "سمگا",
+    "وخاور",
+    "شتران",
+    "كاريس",
+    "ت ثاميد",
+    "پالايش",
+    "لبخند",
+    "تپسي",
+    "كارا",
+]
 
 
 def mw():
@@ -69,4 +108,42 @@ def mw():
     df = mw_df.join(ob_df)
     df = df.assign(dt=jdatetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
+    return df
+
+
+def option_data():
+    mw_df = mw()
+
+    ua_df = mw_df[(mw_df.ticker.isin(ua_list)) & (mw_df.value.astype(int) > 0)][
+        ["ticker", "final"]
+    ]
+    ua_df.rename(columns={"ticker": "ua", "final": "ua_final"}, inplace=True)
+    ua_df.drop_duplicates(inplace=True)
+
+    mw_df = mw_df[mw_df.name.str.startswith("اختيار")]
+
+    def expand_option_info(df):
+        def clean_date(x):
+            date_ = re.findall("[0-9]+", x)
+            date_ = "".join(date_)
+            if len(date_) == 8:
+                return f"{date_[:4]}-{date_[4:6]}-{date_[6:8]}"
+            elif len(date_) == 6:
+                return f"00{date_[:2]}-{date_[2:4]}-{date_[4:6]}"
+            else:
+                print("bad data!")
+                return None
+
+        def clean_ua(x):
+            return x.replace("اختيارخ ", "").replace("اختيارف ", "")
+
+        df[["ua", "strike_price", "ex_date"]] = df.name.str.split("-", expand=True)
+        df = df.assign(ua=df.ua.map(clean_ua))
+        df = df.merge(ua_df, on="ua", how="inner")
+        df = df[~df.ex_date.isnull()]
+        df = df.assign(ex_date=df.ex_date.map(clean_date))
+        return df
+
+    df = expand_option_info(mw_df)
+    df = df.apply(pd.to_numeric, errors="ignore")
     return df
